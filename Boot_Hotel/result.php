3026b0c680a873fc5689cf3929a90cd4b1cedd6c
KelvinDesktop <39939811+KerubinRyu@users.noreply.github.com>
2021-01-01 01:18:19 +0800

    added favicon/changed index

--- Boot_Hotel/result.php
+++ Boot_Hotel/result.php
@@ -9,6 +9,10 @@
         integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
         <!--Local CSS-->
         <link rel="stylesheet" type=text/css href="css/resultStyle.css">
+        <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+        <link rel="manifest" href="images/site.webmanifest">
         <title>Hotel Reservation</title>
     </head>
     <body>
---
+++ Boot_Hotel/search.php
@@ -0,0 +1,3 @@
+<?php
+    header("Location: result.php");
+?>
